73680ca4e6eca686398ee5788de978d602179b44
Arturo <santana3531@yahoo.com>
2017-05-04 18:36:53 -0400

    CrystalGame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,14 @@
+var minNumber = 19;
+var maxNumber = 120;
 
+var randomNum = firstNum(minNumber, maxNumber);
+
+function firstNum(min, max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+console.log(randomNum);
+console.log('hello');
+
+$(".startingNum").html(randomNum);
