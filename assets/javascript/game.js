6c2770854b3db562607f6fc8cb17da88f804e1b9
Arturo <santana3531@yahoo.com>
2017-05-06 11:11:12 -0400

    submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,66 @@
+// Global Variables
 var minNumber = 19;
 var maxNumber = 120;
+var emerald = 0;
+var diamond = 0;
+var morganite = 0;
+var tanzanite = 0;
+var totalScore = 0;
 
 var randomNum = firstNum(minNumber, maxNumber);
 
+
+//Declaring functions
 function firstNum(min, max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-console.log(randomNum);
-console.log('hello');
+// This function will set the values for my crystals
+function randomizeCrystals(){
+	emerald = firstNum(1, 12);
+	diamond = firstNum(1, 12);
+	morganite = firstNum(1, 12);
+	tanzanite = firstNum(1, 12);
+	console.log("emerald: " + emerald);
+	console.log("diamond: " + diamond);
+	console.log("morganite: " + morganite);
+	console.log("tanzanite: " + tanzanite);
+}
 
-$(".startingNum").html(firstNum);
-	console.log(firstNum);
+
+
+//Calling functions and main code
+console.log("randomNum: " + randomNum);
+
+
+$(".startingNum").html(randomNum);
+	console.log(randomNum);
+
+$(".emerald").on("click", function(){
+	console.log("emerald was clicked");
+	totalScore = totalScore + emerald;
+	console.log(totalScore);
+})
+
+$(".diamond").on("click", function(){
+	console.log("diamond was clicked");
+	totalScore = totalScore + diamond;
+	console.log(totalScore);
+})
+
+$(".morganite").on("click", function(){
+	console.log("morganite was clicked");
+	totalScore = totalScore + morganite;
+	console.log(totalScore);
+})
+
+$(".tanzanite").on("click", function(){
+	console.log("tanzanite was clicked");
+	totalScore = totalScore + tanzanite;
+	console.log(totalScore);
+})
+
+
+// Calling the function that randomizes the crystals
+randomizeCrystals();
